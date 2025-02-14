6d9113577281ad25d9ecfecadfe735870c6e13b5
Anil Pal <anilcodes01@gmail.com>
2025-02-14 19:09:19 +0530

    fixing vercel errors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://shelf-zeta.vercel.app/api',
+  baseURL: 'https://shelf-zeta.vercel.app',
   withCredentials: true,
 });
 
